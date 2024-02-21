dcaf57b40eedbe5aebf224925dbdbf3ba731f56b
nirban07 <nirban012@gmail.com>
2024-02-21 22:21:52 +0530

    day 3 of learning solana

--- generate-keypair/generate-keypair.ts
+++ generate-keypair/generate-keypair.ts
@@ -5,3 +5,11 @@ import { getKeypairFromEnvironment } from '@solana-developers/helpers';
 
 const keypair = getKeypairFromEnvironment("SECRET_KEY")
 console.log(keypair)
+
+
+// import { Keypair } from '@solana/web3.js';
+
+// const keypair = Keypair.generate();
+
+// console.log(`The public key is: `, keypair.publicKey.toBase58());
+// console.log(`The secret key is: `, keypair.secretKey);
